321ca96a3528039d26fcc788de4011d82085fccd
leamur88 <jonathan.leibovich@gmail.com>
2023-04-13 15:16:20 -0500

    See preds, targets

--- script.py
+++ script.py
@@ -152,10 +152,14 @@ def train_epoch(model, data_loader, loss_fn, optimizer, scheduler, n_examples):
 				_, preds = torch.max(outputs, dim=1)
 				loss = loss_fn(outputs, targets_micro)
 				correct_predictions += torch.sum(preds == targets)
+				print("Preds", preds)
+				print("targets", targets)
+				print("correct_predictions", correct_predictions)
+				break
 				losses.append(loss.item())
 				loss.backward()
 				optimizer.microbatch_step()
-
+			break
 			optimizer.step()
 			scheduler.step()
 			optimizer.zero_grad()
