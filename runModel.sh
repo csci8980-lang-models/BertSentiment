92319ed52c315a8d51c30bb6a8be6a409eebbc81
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-21 11:01:59 -0500

    change script

--- runModel.sh
+++ runModel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python script.py --train --dp --sst --layerF --portion=50 --epoch=20 && python script.py --train --dp --layerF --sst --portion=75 --epoch=20 && python script.py --train --dp --sst --paramF --portion=50 --epoch=20 && python script.py --train --dp --paramF --sst --portion=75 --epoch=20
+python script.py --train --dp --sst --paramF --portion=50 --epoch=20 && python script.py --train --dp --paramF --sst --portion=75 --epoch=20
