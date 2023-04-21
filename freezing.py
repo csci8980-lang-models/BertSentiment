e7c42e4f076b1d070d95ad2d213a112b113aa4fd
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-21 11:05:22 -0500

    change script

--- freezing.py
+++ freezing.py
@@ -47,9 +47,12 @@ def freezingModifications(args, model, output_dir):
 	if args.paramF:
 		output_dir = output_dir + "paramF/" + str(args.portion) + "/"
 		parameters = []
-		for layer in model.bert.encoder.layer:
-			for param in layer.parameters():
-				parameters.append(param)
+		# for layer in model.bert.encoder.layer:
+		# 	for param in layer.parameters():
+		# 		parameters.append(param)
+
+		for param in model.parameters():
+			parameters.append(param)
 
 		count = int(len(parameters) * PORTION)
 		subset = random.sample(parameters, count)
