104f92644f5bf546f9e8947219e98c0ebbba393c
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-21 10:58:07 -0500

    see if it still throws an error

--- freezing.py
+++ freezing.py
@@ -51,12 +51,12 @@ def freezingModifications(args, model, output_dir):
 		# 	for param in layer.parameters():
 		# 		parameters.append(param)
 		#
-		# for param in model.parameters():
-		# 	parameters.append(param)
-		#
-		# count = int(len(parameters) * PORTION)
-		# subset = random.sample(parameters, count)
-		# for param in subset:
-		# 	param.requires_grad = False
+		for param in model.parameters():
+			parameters.append(param)
+
+		count = int(len(parameters) * PORTION)
+		subset = random.sample(parameters, count)
+		for param in subset:
+			param.requires_grad = False
 
 	return model, output_dir
