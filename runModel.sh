d50a3452053d21f4ba592e6328c20416cfca4269
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-25 11:42:45 -0500

    run model

--- runModel.sh
+++ runModel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python script.py --train --dp --sst --epoch=20 && python script.py --freeze --train --dp --sst --epoch=20
+python script.py --train --sst --epoch=20 && python script.py --freeze --train --sst --epoch=20 && python script.py --freeze --layerF --portion=50 --train --sst --epoch=20 && python script.py --freeze --layerF --portion=75 --train --sst --epoch=20
